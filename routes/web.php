f2cb9b528e7b9b52f7924cf5f4fcdb2fdcc1f7e7
ashton morgan <luckyguy73@yahoo.com>
2016-10-15 09:23:09 -0700

    set up queue

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,11 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/jobs', function () {
+    dispatch(new App\Jobs\LogSomething);
+});
+
+
 Auth::routes();
 
 Route::get('/home', 'HomeController@index');
