60a6c6b9766d84e2ffc75f924878a2f5c6a30ef9
ashton morgan <luckyguy73@yahoo.com>
2016-10-14 23:51:46 -0700

    updated welcome title

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Belle Moda
                 </div>
 
                 <div class="links">
