8253e89cb431552cc9a996760b400fa194d43154
ashton morgan <luckyguy73@yahoo.com>
2016-10-23 12:15:17 -0700

    add social media icons to dashboard

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -10,6 +10,16 @@
                 <div class="panel-body">
                     You are logged in!
                 </div>
+                <div class="col-md-8">
+                    <a href="https://www.facebook.com/" title="facebook" target="_blank"><img src="images/facebook.png" width="36" height="36" alt="facebook"></a>
+                    <a href="https://www.youtube.com" title="youtube" target="_blank"><img src="images/youtube.png" width="36" height="36" alt="youtube"></a>
+                    <a href="https://twitter.com/" title="twitter" target="_blank"><img src="images/twitter.png" width="36" height="36" alt="twitter"></a>
+                    <a href="https://plus.google.com/" title="google+" target="_blank"><img src="images/google.png" width="36" height="36" alt="google plus"></a>
+                    <a href="https://www.instagram.com" title="instagram" target="_blank"><img src="images/instagram.png" width="36" height="36" alt="instagram"></a>
+                    <a href="https://www.pinterest.com/" title="pinterest" target="_blank"><img src="images/pinterest.png" width="36" height="36" alt="pinterest"></a>
+                    <a href="https://www.tumblr.com/" title="tumblr" target="_blank"><img src="images/tumblr.png" width="36" height="36" alt="tumblr"></a>
+                    <a href="https://www.linkedin.com/" title="linkedin" target="_blank"><img src="images/linkedin.png" width="36" height="36" alt="linkedin"></a>
+                </div>
             </div>
         </div>
     </div>
