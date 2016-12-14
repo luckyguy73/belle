991eae12dbf37e685e88ec71a0b2b80ed830ba66
ashton morgan <luckyguy73@yahoo.com>
2016-12-13 22:52:43 -0800

    adjust meta scale for weather page

--- resources/views/weather.blade.php
+++ resources/views/weather.blade.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=0.5">
+    <meta name="viewport" content="width=device-width, initial-scale=0.55">
     <title>Get Weather</title>
     <link rel="stylesheet" href="css/getweather.css" type="text/css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
